f62206a3a4a886f4e7cd56a9758eb30a8bd069e6
Arman Bilge <armanbilge@gmail.com>
2016-05-30 21:53:59 +1200

    Create routes for live pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
+
   def index
   end
 
   def test
   end
+
+  def conference
+    @conference = Conference.find(params[:id])
+  end
+
+  def room
+    @room = Room.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   root 'pages#index'
 
-  get 'test', to: 'pages#test'
+  get '/test', to: 'pages#test'
+
+  get '/live/:id', to: 'pages#conference'
+  get '/live/r/:id', to: 'pages#room'
 
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy', as: 'signout'
