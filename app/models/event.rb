276ac589300fc58288bc0c1bc5b7f424b1494a1a
Arman Bilge <armanbilge@gmail.com>
2016-05-31 19:33:32 +1200

    Achieved minimum working product

--- app/models/event.rb
+++ app/models/event.rb
@@ -23,4 +23,32 @@ class Event < ActiveRecord::Base
     self.datetime = datetime&.beginning_of_minute
   end
 
+  def start_time
+    room.conference.get_time_zone.parse(datetime.asctime)
+  end
+
+  def warning_time
+    room.conference.get_time_zone.parse(datetime.asctime).advance(minutes: event_type.warning_time) unless event_type.warning_time.nil?
+  end
+
+  def passing_time
+    room.conference.get_time_zone.parse(datetime.asctime).advance(minutes: event_type.passing_time) unless event_type.passing_time.nil?
+  end
+
+  def chime_js(now)
+    js = ''
+    puts 'now'
+    puts now
+    puts 'start'
+    puts start_time
+    js += delayed_js('start-chime', start_time - now) if start_time - now >= 0
+    js += delayed_js('warning-chime', warning_time - now) if warning_time and warning_time - now >= 0
+    js += delayed_js('passing-chime', passing_time - now) if warning_time and passing_time - now >= 0
+    js
+  end
+
+  def delayed_js(id, delay)
+    "setTimeout(function() { $('##{id}').prop('currentTime', 0); $('##{id}').trigger('play')}, #{delay.in_milliseconds})"
+  end
+
 end
